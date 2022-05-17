92f4079eda77168e992e0313c1ce4938703585e7
Anthony Côté <104845768+anthony-cote@users.noreply.github.com>
2022-05-17 16:18:10 -0400

    Better formatting and removed superfluous variable assignments

--- code/LEI/LEI.ino
+++ code/LEI/LEI.ino
@@ -1,36 +1,38 @@
-int TTL_in  = 7;
-int TTL_out = 10;
-float duty  = 30;  //duty cycle in % DO NOT EXCEED 40
-int trigger = 0; //value of trigger signal
-int dwell   = 3; //dwell time in ms %DO NOT EXCEED 9
-int through = 10; //low time in ms
-int count   = 100; //number of sparks to generate
-int num     = 0;
+int TTL_in = 7;
+int TTL_out = 4;
+float duty = 30;  //Duty cycle in %. DO NOT EXCEED 40.
+int trigger = 0; //Value of trigger signal.
+int dwell = 3; //Dwell time in ms. DO NOT EXCEED 9.
+int through; //Low time in ms.
+int count = 100; //Number of sparks to generate.
+int num = 0;
+
 void setup() {
+
   pinMode(TTL_in, INPUT);
   pinMode(TTL_out, OUTPUT);
-  digitalWrite(TTL_out, LOW);
-  trigger = 0;
-  through = ceil(dwell*100/duty - dwell);
+
+  through = ceil(dwell * 100 / duty - dwell);
 }
 
 void loop() {
 
   trigger = digitalRead(TTL_in);
-  
-  if (trigger) {
-    num = 0;
-    while (num<count){
-      digitalWrite(TTL_out,HIGH);
-      delay(dwell);
-      digitalWrite(TTL_out,LOW);
-      delay(through);
-      num++;
-    }
-    delay(2000);
-  }
-  
-  
 
-  
+  if (trigger) {
+
+    while (num < count) {
+
+      digitalWrite(TTL_out, HIGH);
+      
+      delay(dwell);
+      
+      digitalWrite(TTL_out, LOW);
+      
+      delay(through);
+      
+      num ++;
+    }
+    delay(4000);
+  }
 }
